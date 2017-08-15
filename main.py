d14dd7c16ae04265dad95d6edb0c5edc29e2614b
Corbin Gruber <corbingrbr@gmail.com>
2017-08-14 19:45:32 -0700

    simple main function with import of player class

---
+++ main.py
@@ -0,0 +1,9 @@
+from player import Player
+
+if __name__ == "__main__":
+
+    player = Player("Corbin", 10)
+    player2 = Player("Cody", 0)
+
+    print player.name, "has a skill level of ", player.skill 
+    print player2.name, "has a skill level of ", player2.skill 
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+
+    def __init__(self, name, skill):
+        self.name = name
+        self.skill = skill
